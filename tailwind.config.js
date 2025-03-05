57e588bc95193304b9649a2b00ba9be7fe9330e4
sabreen01 <Sabreen.20377412@compit.aun.edu.eg>
2025-03-05 02:55:14 +0200

    init com

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,ts,jsx,tsx}",
+    './public/index.html',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'main-color': 'rgb(250 136 54)',
+        'main-dark-color': '#CF5600',
+        'main-orange-color': '#FA8836',
+         'main-green-color': '#128000'
+      },
+      clipPath: {
+        rhombus: "polygon(50% 0%, 100% 50%, 50% 100%, 0% 50%)",
+      },
+      animation: {
+        fadeIn: "fadeIn 0.3s ease-out forwards",
+      },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: 0, transform: "scale(0.95)" },
+          "100%": { opacity: 1, transform: "scale(1)" },
+        },
+      },
+    },
+  },
+  plugins: [],
+}
+
